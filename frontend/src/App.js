d2d367f71af4c45dd0bd7bfea152753df9b3aa63
Vladislav Betivu <vladislav.betivu@ellation.com>
2020-06-28 18:14:13 +0300

    create app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,27 @@
-import React from 'react';
+import React from "react";
 
-function App() {
-  return <div>Implement me!</div>;
-}
+import StoreProvider from "./store/context";
+import Preview from "./components/Preview";
+import SendButton from "./components/SendButton";
+import Status from "./components/Status";
+import DropZone from "./components/DropZone";
+import FileInput from "./components/FileInput";
+
+const App = () => (
+  <StoreProvider>
+    <DropZone className="app">
+      <div className="content-wrapper">
+        <div className="status-wrapper">
+          <Status />
+        </div>
+        <div className="controls-wrapper">
+          <FileInput />
+          <SendButton />
+        </div>
+        <Preview />
+      </div>
+    </DropZone>
+  </StoreProvider>
+);
 
 export default App;
