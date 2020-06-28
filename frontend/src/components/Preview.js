ee85925b7134786af64c0007d5e9eb08e12681e0
Vladislav Betivu <vladislav.betivu@ellation.com>
2020-06-28 18:11:54 +0300

    create preview

---
+++ frontend/src/components/Preview.js
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+
+import { Store } from "../store/context";
+
+const Preview = () => {
+  const { files, removeFile } = useContext(Store);
+
+  return (
+    <ul className="files-list">
+      {files.length ? (
+        files.map(({ name, value }, index) => (
+          <li className="list-item" key={index}>
+            <p>
+              {name} - {new Set(value).size} unique email addresses
+            </p>
+            <button onClick={() => removeFile(name)}>Remove</button>
+          </li>
+        ))
+      ) : (
+        <li className="list-item">Choose or drop &quot;.txt&quot; files</li>
+      )}
+    </ul>
+  );
+};
+
+export default Preview;
